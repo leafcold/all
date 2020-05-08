d1f8e6693ded506785af60b29c0424cc2b7f8be5
yeegao <ye_gao@163.com>
2020-05-08 15:10:45 +0800

    From YG test Git

--- server/src/main/java/com/first/core/net/UdpNetty.java
+++ server/src/main/java/com/first/core/net/UdpNetty.java
@@ -10,7 +10,7 @@ import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.bootstrap.Bootstrap;
 public class UdpNetty {
     public static void main(String[] args) {
-        System.out.println("测试git");
+        System.out.println("测试git2");
     }
 
 }

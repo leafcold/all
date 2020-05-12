487544dcb48345ccaf8646ecaa0289de32a9342a
yeegao <ye_gao@163.com>
2020-05-12 10:15:39 +0800

    去掉编译错误

--- server/src/test/java/Main.java
+++ server/src/test/java/Main.java
@@ -2,7 +2,6 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import first.com.protocol.move.PersonMove;
 
 
-
 public class Main {
     public static void main(String[] args) throws InvalidProtocolBufferException {
 
